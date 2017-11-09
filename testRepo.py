96175165175f89cd09b951a1a4ecbd5aeb21fb59
Jack Ranado <ranado@spectre.sdsu.edu>
2017-11-09 15:55:12 -0800

    Revert "HII Paul <3"
    This reverts commit 59317711e7eda467b75d3ba419f7bac74d2dad96.

--- testRepo.py
+++ testRepo.py
@@ -1,12 +1,19 @@
 def printer():
-	print "How do you do?"
+    print "How do you do?"
 
 
 def alexK():
+<<<<<<< HEAD
 	num = 0
 	printer()
 	# Write something useful
 	print "Okay something legit"
 	print "kek"
+=======
+    num = 0
+    printer()
+    # Write something useful
+>>>>>>> parent of 5931771... HII Paul <3
+
 
 alexK()
