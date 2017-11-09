00c15b01b18e65d8d213358d27843b5d17016fa5
Jack Ranado <ranado@spectre.sdsu.edu>
2017-11-09 15:57:40 -0800

    idek..

--- testRepo.py
+++ testRepo.py
@@ -3,17 +3,10 @@ def printer():
 
 
 def alexK():
-<<<<<<< HEAD
 	num = 0
 	printer()
 	# Write something useful
 	print "Okay something legit"
 	print "kek"
-=======
-    num = 0
-    printer()
-    # Write something useful
->>>>>>> parent of 5931771... HII Paul <3
-
 
 alexK()
