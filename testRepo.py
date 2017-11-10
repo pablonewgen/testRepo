248acd83d77a010192233bea3d00a82744343581
Jack Ranado <ranado@spectre.sdsu.edu>
2017-11-09 16:01:40 -0800

    did this work

--- testRepo.py
+++ testRepo.py
@@ -3,10 +3,9 @@ def printer():
 
 
 def alexK():
-	num = 0
-	printer()
-	# Write something useful
-	print "Okay something legit"
-	print "kek"
+    num = 0
+    printer()
+    # Write something useful
+
 
 alexK()
