30127ac960bb473d0690bd122a5dce1f5dd02860
Jack Ranado <ranado@spectre.sdsu.edu>
2017-11-09 17:05:58 -0800

    Back to Paul's stuff

--- testRepo.py
+++ testRepo.py
@@ -1,11 +1,11 @@
 def printer():
-	print "How do you do?"
+    print "How do you do?"
 
 
 def alexK():
-	num = 0
-	printer()
-	# Write something useful
-	print "Your mom"
+    num = 0
+    printer()
+    # Write something useful
+
 
 alexK()
